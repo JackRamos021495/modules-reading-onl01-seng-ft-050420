2e560d6cb5025bb852b55fc90badf99cdb0e6a02
Jackson Ramos <jackramos021495@gmail.com>
2020-05-18 14:09:40 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,10 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
+<<<<<<< HEAD
   extend MetaDancing
+=======
+>>>>>>> 414385366694c747417965b7d708fb2d2625f9f8
   include Dance
 
   attr_accessor :name
